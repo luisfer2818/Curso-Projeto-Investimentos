8c733e80ce850ba8e162ff1a8410584004b0ac9a
fernando <luisfernando1011@hotmail.com>
2018-03-16 18:54:41 -0300

    Menu lateral

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Investindo</title>
-    @yield('css-view')
     <link rel="stylesheet" href="{{ asset('css/stylesheet.css') }}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Fredoka+One">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.css">
+    @yield('css-view')
 </head>
 <body>
+    @include('templates.menu-lateral')
     @yield('conteudo-view')
     @yield('js-view')
 
